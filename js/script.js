2437a2f3444a33a6888caa41063100dbf5c69b24
MariiaPolishchuk <mariia.poli91@gmail.com>
2022-04-16 22:31:46 +0300

    files were changed

--- js/script.js
+++ js/script.js
@@ -18,12 +18,8 @@ nums(1, 9);
 log('---# 5 Часы')
 function clockInit (){
     const tim = new Date();
+    const options = { hour: "2-digit", minute: "2-digit", second: "2-digit", hour12: false };
     let clock = document.getElementById ('clock');
-    clock.innerHTML = tim.getHours()+':'+tim.getMinutes()+':'+tim.getSeconds();
-    }
-    setInterval(clockInit, 1000);
-    clock.addEventListener('click', () => {
-    formatFull = !formatFull;
-    clockInit(); });
-
-   
+    clock.innerHTML = tim.toLocaleTimeString('en', options);
+}
+setInterval(clockInit, 1000);
